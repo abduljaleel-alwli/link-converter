26e292d07ebf5246104649e6be270866b61f020a
abduljaleel-alwli <uljaleel2@gmail.com>
2024-01-06 10:28:41 +0300

    Update Message Element

--- script.js
+++ script.js
@@ -9,21 +9,35 @@ function submitForm(event){
 form.addEventListener('submit', submitForm);
 
 
-const convert_button = document.getElementById('convert-button')
+const convert_button = document.getElementById('convert-button');
+const normal_link = document.getElementById('normal-link');
+const message = document.getElementById("message");
+
+normal_link.onkeyup = () => {
+    console.log(';alsdjkf');
+    message.style.display = 'none'
+}
 
 convert_button.addEventListener('click', () => {
-    const normal_link = document.getElementById('normal-link')
-    const converted_link = document.getElementById('converted-link')
-    const newLink = decodeURIComponent(normal_link.value);
-    converted_link.value = newLink;
+    if (normal_link.value.includes('http')) {
+        const converted_link = document.getElementById('converted-link');
+        const newLink = decodeURIComponent(normal_link.value);
+        converted_link.value = newLink;
 
-    converted_link.select();
-    converted_link.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    
-    const message = document.getElementById("message")
-    message.classList.add('message-show')
-    setTimeout(() => {
-        message.classList.add('message-close')
-    }, 5000);
-})
+        converted_link.select();
+        converted_link.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+
+        message.innerHTML = '🤗 تم نسخ الرابط'
+        message.style.display = 'block'
+        setTimeout(() => {
+            message.style.display = 'none'
+        }, 5000);
+    }else{
+        message.innerHTML = '🙃 خطاء في عنوان الرابط'
+        message.style.display = 'block'
+        setTimeout(() => {
+            message.style.display = 'none'
+        }, 5000);
+    }
+});
